1ed96aa27bae59fa9f982361458231cdcfe349f2
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-08-06 12:16:22 +0200

    Updated opi. Added ONAM fields for insertss PVs.

--- startup/userPostDriverConf-gersemi-freia.cmd
+++ startup/userPostDriverConf-gersemi-freia.cmd
@@ -1,3 +1,9 @@
 ####################################################################
 ## User provided PLC or fast controller driver post configuration ##
 ####################################################################
+dbpf CstatV-Ctrl:LiqIns:sInp.ONAM "Liquid Insert"
+dbpf CstatV-Ctrl:LiqIns:sProcInp.ONAM "Liquid Insert"
+dbpf CstatV-Ctrl:MagIns:sInp.ONAM "Magnet Insert"
+dbpf CstatV-Ctrl:MagIns:sProcInp.ONAM "Magnet Insert"
+dbpf CstatV-Ctrl:VacIns:sInp.ONAM "Vacuum Insert"
+dbpf CstatV-Ctrl:VacIns:sProcInp.ONAM "Vacuum Insert"
