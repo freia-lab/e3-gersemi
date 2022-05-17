47fa3ce08ea0074cb33622c9a733dc6570814dfe
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-17 17:45:24 +0200

    Preparations for downloading the CERNOX sensor calibration from Epics.

---
+++ doc/CERNOX/create_lookup_table.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+awk '/TT/ && !/S/ {print $1, $2}' <Affectation\ CERNOX\ GERSEMI.txt | sed '{s/CV//}' |awk '{ if (substr($1,6) == "UU") {t=substr($1,0,5); printf "%sL %s\n",t,$2; printf "%sM %s\n",t,$2; printf "%sV %s\n",t,$2 } else {print $0}}'
+
