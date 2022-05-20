c0a138e4b3ece8beaac8a4a06db99f99d184da14
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-20 22:10:05 +0200

    Finished programs for loading the CERNOX calibration data.

--- tools/CERNOX/generate_calibr_cmds.sh
+++ tools/CERNOX/generate_calibr_cmds.sh
@@ -31,74 +31,76 @@ fi
 
 #### 1.5..4.5 K ####
 
-cmd="caput $P$1:cPolyA -a 5"
+cmd="caput -a $P$1:cPolyA 5"
 f=CX_LS_$2\(1.5K...4.5K
 cf=($f*)
 #echo $cf
 val=`awk -f $AWKPR1 < $cf`
 echo $cmd$val
 rlist1=(`awk -f $AWKPR2 < $cf`)
-echo rlist1=${rlist1[@]}
-tmp=${rlist1[0]}" < "${rlist1[2]}
-simple=`echo "$tmp" |bc -l`
-#if [ $simple -eq 1 ]
-#then
-#    echo "simple"
-#else
-#    echo "complicated"
-#fi
-if [ $simple -eq 0 ]
+#echo rlist1=${rlist1[@]}
+
+if [ ${rlist1[0]} == 1 ]
 then
-    r0=${rlist1[3]}
+    r0=${rlist1[1]}
 fi
 
 #### 4.5..20.0 K ####
 
-cmd="caput $P$1:cPolyB -a 5"
+cmd="caput -a $P$1:cPolyB 5"
 f=CX_LS_$2\(4.5K...20.0K
 cf=($f*)
 val=`awk -f $AWKPR1 < $cf`
 echo $cmd$val
 rlist2=(`awk -f $AWKPR2 < $cf`)
-echo rlist2=${rlist2[@]}
-if [ $simple -eq 1 ]
+#echo rlist2=${rlist2[@]}
+if [  ${rlist2[0]} == 0 ]
 then
-    r0=${rlist2[3]}
+    r0=${rlist2[1]}
 else
-    r1=${rlist2[2]}
+    r1=${rlist2[1]}
+    err_r1=${rlist2[2]}
 fi
 
 #r0=`tail -1 $cf | awk '{printf " %13.7f", $1}'`
 
 #### 20.0..100.0 K ####
 
-cmd="caput $P$1:cPolyC -a 5"
+cmd="caput -a $P$1:cPolyC 5"
 f=CX_LS_$2\(20.0K...100.0K
 cf=($f*)
 val=`awk -f $AWKPR1 < $cf`
 echo $cmd$val
 rlist3=(`awk -f $AWKPR2 < $cf`)
-echo rlist3=${rlist3[@]}
-if [ $simple -eq 1 ]
+#echo rlist3=${rlist3[@]}
+if [ ${rlist3[0]} == 0 ]
 then
-    r1=${rlist3[3]}
+    r1=${rlist3[1]}
+else
+    err_tmp=${rlist3[2]}
+    cmp=$err_tmp" < "$err_r1
+    res=`echo "$cmp" |bc -l`
+    if [ $res -eq 1 ]
+    then
+	r1=${rlist3[1]}
+    fi
 fi
 #r1=`tail -1 $cf | awk '{printf " %13.7f", $1}'`
 
 #### 100.0..310.0 K ####
 
-cmd="caput $P$1:cPolyD -a 5"
+cmd="caput -a $P$1:cPolyD 5"
 f=CX_LS_$2\(100.0K...310.0K
 cf=($f*)
 val=`awk -f $AWKPR1 < $cf`
 echo $cmd$val
 rlist=(`awk -f $AWKPR2 < $cf`)
 #echo rlist=${rlist[@]}
-if [ $simple -eq 1 ]
+if [ ${rlist[0]} == 0 ]
 then
-    r2=${rlist[3]}
+    r2=${rlist[1]}
 else
-    r2=${rlist[0]}
+    r2=${rlist[1]}
 fi
 #r2=`tail -1 $cf | awk '{printf " %13.7f", $1}'`
 
--- tools/CERNOX/get_coeff.awk
+++ tools/CERNOX/get_coeff.awk
@@ -1,4 +1,4 @@
 # Extract the polynomial coefficients A0 to A4 from the calibration file
 #
-/A[01234]=/ {printf " %16.10f", $2 }
+/A[01234]=/ {printf " %16.6f", $2 }
 END { printf "\n" }
